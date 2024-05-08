5fd5dc235edf6647124f4d26f60b578789fc28fb
winxpqq955 <gwinxpqq955@gmail.com>
2024-05-08 20:20:37 +0800

    fix: IOEvents constructor

--- younkoo-client/src/base/render/gui/input/IOEvents.h
+++ younkoo-client/src/base/render/gui/input/IOEvents.h
@@ -16,7 +16,8 @@ namespace YounkooIO {
 
 	struct IOEvent {
 		EventType type;
-    	virtual ~IOEvent() {}
+		IOEvent(EventType type) : type(type) {}
+		virtual ~IOEvent() {}
 	};
 
 	struct KeyEvent final : IOEvent
@@ -29,7 +30,7 @@ namespace YounkooIO {
 
 	struct MousePosEvent final : IOEvent
 	{
-		constexpr MousePosEvent(double posX = 0, double posY = 0) noexcept
+		MousePosEvent(double posX = 0, double posY = 0) noexcept
 			: IOEvent{ EventType::MOUSEPOS }, posX(posX), posY(posY) {}
 		double posX;
 		double posY;
@@ -37,7 +38,7 @@ namespace YounkooIO {
 
 	struct MouseEvent final : IOEvent
 	{
-		constexpr MouseEvent(int button = 0, int action = 0) noexcept
+		MouseEvent(int button = 0, int action = 0) noexcept
 			: IOEvent{ EventType::MOUSE }, button(button), action(action) {}
 		int button;
 		int action;
@@ -45,7 +46,7 @@ namespace YounkooIO {
 
 	struct WheelEvent final : IOEvent
 	{
-		constexpr WheelEvent(double offsetX = 0, double offsetY = 0) noexcept :
+		WheelEvent(double offsetX = 0, double offsetY = 0) noexcept :
 			IOEvent{ EventType::WHEEL }, offsetX(offsetX), offsetY(offsetY) {}
 		double offsetX;
 		double offsetY;
@@ -53,7 +54,7 @@ namespace YounkooIO {
 
 	struct CharEvent final :IOEvent
 	{
-		constexpr CharEvent(unsigned int codepoint = 0) noexcept
+		CharEvent(unsigned int codepoint = 0) noexcept
 			: IOEvent{ EventType::TYPE }, codepoint(codepoint) {}
 		unsigned int codepoint;
 	};
