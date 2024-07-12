747707d51bb2ec3d017d714aa7f0006643bab453
Baier <68586144+baier233@users.noreply.github.com>
2024-07-12 14:02:33 +0800

    Lol

--- younkoo-client/src/base/GitInfo.h
+++ younkoo-client/src/base/GitInfo.h
@@ -1,3 +1,3 @@
 #pragma once
-#define GIT_COMMIT_HASH "a09e34a"
-#define GIT_COMMIT_TIME "Fri Jul 12 13:12:40 2024 +0800"
+#define GIT_COMMIT_HASH "faf5ee4"
+#define GIT_COMMIT_TIME "Fri Jul 12 13:40:49 2024 +0800"
