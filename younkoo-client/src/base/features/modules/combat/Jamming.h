ca059241a754bf19c0e704ddc37175a72ad9927b
Baier <68586144+baier233@users.noreply.github.com>
2024-07-16 04:15:43 +0800

    stuff

---
+++ younkoo-client/src/base/features/modules/combat/Jamming.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "../AbstractModule.h"
+#include "../../../Younkoo.hpp"
+#include <GL/glew.h>
+#include <nanovg.h>
+#include <wrapper/net/minecraft/client/Minecraft.h>
+class Jamming : public AbstractModule
+{
+public:
+	static Jamming& getInstance();
+	void onEnable();
+	void onDisable();
+	void onUpdate();
+protected:
+	Jamming();
+};
---
+++ younkoo-client/src/base/sdk/hook/hooks/PlayerHook.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "../AbstractHook.h"
+class PlayerHook : public AbstractHook
+{
+public:
+	void hook(const HookManagerData& container) override;
+private:
+
+};
+
